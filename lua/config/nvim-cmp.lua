10199a634b52cb7f35a22ca0e5d141e73bf1389b
lihao <lihao@fabu.ai>
2022-04-24 17:42:05 +0800

    Fix cmp mapping in command mode

--- lua/config/nvim-cmp.lua
+++ lua/config/nvim-cmp.lua
@@ -16,9 +16,12 @@ cmp.setup({
       vim.fn["UltiSnips#Anon"](args.body) -- For `ultisnips` users.
     end,
   },
+  -- i: insert
+  -- c: command
+  -- n: normal
   mapping = {
-    ['<C-p>'] = cmp.mapping.select_prev_item(),
-		['<C-n>'] = cmp.mapping.select_next_item(),
+    ['<C-p>'] = cmp.mapping(cmp.mapping.select_prev_item(), { 'i', 'c' }),
+		['<C-n>'] = cmp.mapping(cmp.mapping.select_next_item(), { 'i', 'c' }),
     ['<C-b>'] = cmp.mapping(cmp.mapping.scroll_docs(-4), { 'i', 'c' }),
     ['<C-f>'] = cmp.mapping(cmp.mapping.scroll_docs(4), { 'i', 'c' }),
     ['<C-Space>'] = cmp.mapping(cmp.mapping.complete(), { 'i', 'c' }),
