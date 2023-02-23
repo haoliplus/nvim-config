4d4855e24d38404670eaa822ab83b2857f4b31ab
lihao <lihao@fabu.ai>
2023-02-23 11:57:19 +0800

    Add python projects

---
+++ myprojects-temp/py_proj/setup.py
@@ -0,0 +1,15 @@
+#! /usr/bin/env python3
+# -*- coding: utf-8 -*-
+# vim:fenc=utf-8
+#
+# Copyright © 2022 lihao <haoliplus@gmail.com>
+#
+# Distributed under terms of the MIT license.
+"""
+demo
+"""
+
+from setuptools import setup
+
+if __name__ == "__main__":
+    setup()
---
+++ myprojects-temp/py_proj/tests/demo_test.py
@@ -0,0 +1,37 @@
+#! /usr/bin/env python3
+# -*- coding: utf-8 -*-
+# vim:fenc=utf-8
+#
+# Copyright © 2023 lihao <lihao@fabu.ai>
+#
+# Distributed under terms of the MIT license.
+
+"""
+
+"""
+import unittest
+import yaml
+
+# python >= 3.7
+import importlib.resources as importlib_resources
+
+
+class TestStringMethods(unittest.TestCase):
+    def test_config(self):
+        pkg = importlib_resources.files("demo")
+        default_config_file = pkg / "resources" / "default_config.yaml"
+        config_str = None
+        with default_config_file.open() as fp:
+            config_str = fp.read()
+        self.assertIsNotNone(config_str)
+        # config_str = default_config_file.open().read()
+        config = yaml.load(config_str, Loader=yaml.SafeLoader)
+        self.assertIsNotNone(config)
+
+    def test_isupper(self):
+        self.assertTrue("FOO".isupper())
+        self.assertFalse("Foo".isupper())
+
+
+if __name__ == "__main__":
+    unittest.main()
