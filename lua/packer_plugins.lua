c4d812d624e0b586a3aac1680f0f4ae314e896c3
lihao <haoliplus@gmail.com>
2023-03-16 02:05:45 +0800

    Update vim tab with tree

--- lua/packer_plugins.lua
+++ lua/packer_plugins.lua
@@ -220,4 +220,51 @@ packer.startup(function(use)
       }
     end
   }
+  use {'romgrk/barbar.nvim', requires = 'nvim-tree/nvim-web-devicons',
+    config = function() 
+      local map = vim.api.nvim_set_keymap
+      local opts = { noremap = true, silent = true }
+      -- Move to previous/next
+      -- map('n', '<A-,>', '<Cmd>BufferPrevious<CR>', opts)
+      -- map('n', '<A-.>', '<Cmd>BufferNext<CR>', opts)
+      -- Re-order to previous/next
+      -- map('n', '<A-<>', '<Cmd>BufferMovePrevious<CR>', opts)
+      -- map('n', '<A->>', '<Cmd>BufferMoveNext<CR>', opts)
+      -- Goto buffer in position...
+      map('n', '<Leader>1', '<Cmd>BufferGoto 1<CR>', opts)
+      map('n', '<Leader>2', '<Cmd>BufferGoto 2<CR>', opts)
+      map('n', '<Leader>3', '<Cmd>BufferGoto 3<CR>', opts)
+      map('n', '<Leader>4', '<Cmd>BufferGoto 4<CR>', opts)
+      map('n', '<Leader>5', '<Cmd>BufferGoto 5<CR>', opts)
+      map('n', '<Leader>6', '<Cmd>BufferGoto 6<CR>', opts)
+      map('n', '<Leader>7', '<Cmd>BufferGoto 7<CR>', opts)
+      map('n', '<Leader>8', '<Cmd>BufferGoto 8<CR>', opts)
+      map('n', '<Leader>9', '<Cmd>BufferGoto 9<CR>', opts)
+      map('n', '<Leader>0', '<Cmd>BufferLast<CR>', opts)
+      -- Pin/unpin buffer
+      map('n', '<A-p>', '<Cmd>BufferPin<CR>', opts)
+      -- Close buffer
+      map('n', '<Leader>c', '<Cmd>BufferClose<CR>', opts)
+      -- Wipeout buffer
+      --                 :BufferWipeout
+      -- Close commands
+      --                 :BufferCloseAllButCurrent
+      --                 :BufferCloseAllButPinned
+      --                 :BufferCloseAllButCurrentOrPinned
+      --                 :BufferCloseBuffersLeft
+      --                 :BufferCloseBuffersRight
+      -- Magic buffer-picking mode
+      -- map('n', '<C-p>', '<Cmd>BufferPick<CR>', opts)
+      -- Sort automatically by...
+      -- map('n', '<Space>bb', '<Cmd>BufferOrderByBufferNumber<CR>', opts)
+      -- map('n', '<Space>bd', '<Cmd>BufferOrderByDirectory<CR>', opts)
+      -- map('n', '<Space>bl', '<Cmd>BufferOrderByLanguage<CR>', opts)
+      -- map('n', '<Space>bw', '<Cmd>BufferOrderByWindowNumber<CR>', opts)
+    end
+  }
+  use {'akinsho/bufferline.nvim', tag = "v3.*", requires = 'nvim-tree/nvim-web-devicons',
+  setup = function() 
+     animation = true
+  end
+}
 end)
--- lua/plugins/nvim-tree.lua
+++ lua/plugins/nvim-tree.lua
@@ -154,27 +154,27 @@ end
 --         end
 --     end
 -- })
-vim.api.nvim_create_autocmd("BufEnter", {
-  group = vim.api.nvim_create_augroup("NvimTreeClose", {clear = true}),
-  pattern = "NvimTree_*",
-  callback = function()
-    -- winlayout: The result is a nested List containing the layout of windows in a tabpage.
-    -- For a leaf window, it returns:
-    --     ["leaf", {winid}]
-    local layout = vim.api.nvim_call_function("winlayout", {})
-    if layout[1] == "leaf" and 
-      vim.api.nvim_buf_get_option(vim.api.nvim_win_get_buf(layout[2]), "filetype") == "NvimTree" 
-      and layout[3] == nil then 
-        vim.cmd("confirm quit")
-     end
-  end
-})
+-- vim.api.nvim_create_autocmd("BufEnter", {
+--   group = vim.api.nvim_create_augroup("NvimTreeClose", {clear = true}),
+--   pattern = "NvimTree_*",
+--   callback = function()
+--     -- winlayout: The result is a nested List containing the layout of windows in a tabpage.
+--     -- For a leaf window, it returns:
+--     --     ["leaf", {winid}]
+--     local layout = vim.api.nvim_call_function("winlayout", {})
+--     if layout[1] == "leaf" and 
+--       vim.api.nvim_buf_get_option(vim.api.nvim_win_get_buf(layout[2]), "filetype") == "NvimTree" 
+--       and layout[3] == nil then 
+--         vim.cmd("confirm quit")
+--      end
+--   end
+-- })
 
 -- change the tab title when leave the tab
-vim.api.nvim_create_autocmd({ "TabLeave" }, 
-  { 
-    pattern = "NvimTree_*",
-    callback = function()
-        vim.cmd "wincmd p"
-    end
-  })
+-- vim.api.nvim_create_autocmd({ "TabLeave" }, 
+--   { 
+--     pattern = "NvimTree_*",
+--     callback = function()
+--         vim.cmd "wincmd p"
+--     end
+--   })
