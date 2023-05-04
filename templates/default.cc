9e15e37f3c60a5125f486d31b4702003b2c423b7
lihao <lihao@fabu.ai>
2023-05-04 15:56:39 +0800

    Update

--- templates/default.cc
+++ templates/default.cc
@@ -1,1 +1,7 @@
-default.cpp
+/**
+ * @author      : {{_author_}} ({{_email_}})
+ * @file        : {{_file_name_}}
+ * @created     : {{_date_}}
+ */
+
+#include "{{_file_name_}}.h"
--- templates/main.cc
+++ templates/main.cc
@@ -1,1 +1,13 @@
-main.cpp
+/**
+ * @author      : {{_author_}} ({{_email_}})
+ * @file        : {{_file_name_}}
+ * @created     : {{_date_}}
+ */
+
+#include <iostream>
+
+int main()
+{
+    {{_cursor_}}
+    return 0;
+}
