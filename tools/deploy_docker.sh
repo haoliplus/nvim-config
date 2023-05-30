227bb718ce756ef30f93f2f7ab427718768829e2
lihao <haoliplus@gmail.com>
2023-05-30 14:57:40 +0800

    Update tmux

--- tools/deploy_docker.sh
+++ tools/deploy_docker.sh
@@ -8,16 +8,16 @@
 set -ex
 
 sync_file() {
-  docker exec  -it ${CONTAINER} mkdir -p /home/${USER}/$(dirname $1)/
-  docker cp -L  $1 ${CONTAINER}:/home/${USER}/$(dirname $1)/
+  docker exec  -u ${USER} -it ${CONTAINER} mkdir -p /home/${USER}/.cache/docker/$(dirname $1)/
+  docker cp -L  $1 ${CONTAINER}:/home/${USER}/.cache/docker/$(dirname $1)/
 }
 
-docker exec  -it ${CONTAINER} sudo apt update
-docker exec  -it ${CONTAINER} sudo apt install -y ninja-build gettext cmake unzip curl
-docker exec -w /home/${USER} -it ${CONTAINER} git clone https://github.com/neovim/neovim
-docker exec -w /home/${USER} -it ${CONTAINER} git checkout v0.8.3
-docker exec -w /home/${USER} -it ${CONTAINER} make
-docker exec -w /home/${USER} -it ${CONTAINER} sudo make install
+docker exec -u ${USER} \
+  -it ${CONTAINER} sudo apt update
+docker exec -u ${USER} \
+  -it ${CONTAINER} sudo apt install -y ninja-build gettext cmake unzip curl
+docker exec -u ${USER} \
+  -it ${CONTAINER} mkdir -p /home/${USER}/.cache/docker
 
 cd ${HOME}
 sync_file .dotfiles/nvim
@@ -26,3 +26,5 @@ sync_file .local/bin/nvim.appimage
 sync_file .local/bin/node
 sync_file .local/lib/nvim
 sync_file .local/share/nvim
+sync_file .tmux.conf
+sync_file .tmux.conf.local
