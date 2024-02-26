171c232c49c6de4f830af98fb53efb6d1a495e4e
Hao Li <haoliplus@gmail.com>
2024-02-26 20:11:14 +0800

    Add keymap

--- lua/plugins/navigator.lua
+++ lua/plugins/navigator.lua
@@ -86,6 +86,8 @@ return {
 			vim.keymap.set("n", "<leader>ff", builtin.find_files, {})
 			vim.keymap.set("n", "<leader>fg", builtin.live_grep, {})
 			vim.keymap.set("n", "<leader>fr", builtin.oldfiles, {})
+			vim.keymap.set("n", "<c-f>", builtin.find_files, {})
+	--		vim.g.ctrlp_map = "<c-f>"
 		end,
 	},
 }
