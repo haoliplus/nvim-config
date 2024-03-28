1dbc933c6f5a0982bde072ed39545351b2d091de
Hao Li <haoliplus@gmail.com>
2024-03-28 12:03:31 +0800

    Add hilight

--- lua/plugins/editor.lua
+++ lua/plugins/editor.lua
@@ -1,4 +1,20 @@
 return {
+  {
+      'tzachar/local-highlight.nvim',
+      config = function()
+        require('local-highlight').setup({
+            -- file_types = {'python', 'cpp'}, -- If this is given only attach to this
+            -- OR attach to every filetype except:
+            disable_file_types = {'txt'},
+            -- hlgroup = 'Search',
+            -- cw_hlgroup = nil,
+            -- Whether to display highlights in INSERT mode or not
+            insert_mode = false,
+            min_match_len = 1,
+            max_match_len = math.huge,
+        })
+      end
+  },
 	-- quick commentary
 	-- { "tpope/vim-commentary" },
 	{
