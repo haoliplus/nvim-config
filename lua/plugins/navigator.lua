d80a0141a648bfe2e15e8a07e2aabf025d7a6aa5
Hao Li <haoliplus@gmail.com>
2024-09-04 23:49:43 +0800

    Update buf

--- lua/plugins/navigator.lua
+++ lua/plugins/navigator.lua
@@ -101,5 +101,17 @@ return {
   "chentoast/marks.nvim",
     event = "VeryLazy",
     opts = {},
-  }
+  },
+  {
+    'crusj/bookmarks.nvim',
+    keys = {
+        { "<tab><tab>", mode = { "n" } },
+    },
+    branch = 'main',
+    dependencies = { 'nvim-web-devicons' },
+    config = function()
+        require("bookmarks").setup()
+        require("telescope").load_extension("bookmarks")
+    end
+}
 }
