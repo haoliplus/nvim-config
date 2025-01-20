c4af503b08f0a5ec946c1893c095bdb12c889a53
Hao Li <haoliplus@gmail.com>
2025-01-20 23:55:46 +0800

    Using github again

--- lua/plugins/copilot.lua
+++ lua/plugins/copilot.lua
@@ -62,35 +62,35 @@ return {
   {
     "yetone/avante.nvim",
     event = "VeryLazy",
-    enabled = false,
+    -- enabled = false,
     lazy = false,
     version = false, -- set this if you want to always pull the latest change
     opts = {
-      provider = "deepseek",
+      -- provider = "deepseek",
       -- auto_suggestions_provider="deepseek",
-      vendors = {
-        deepseek = {
-          __inherited_from = "openai",
-          api_key_name = "DEEPSEEK_API_KEY",
-          endpoint = "https://api.deepseek.com/",
-          model = "deepseek-chat",
-        },
-
-      },
+      -- vendors = {
+      --   deepseek = {
+      --     __inherited_from = "openai",
+      --     api_key_name = "DEEPSEEK_API_KEY",
+      --     endpoint = "https://api.deepseek.com/",
+      --     model = "deepseek-chat",
+      --   },
+      --
+      -- },
       -- add any opts here
       -- @alias Provider "claude" | "openai" | "azure" | "gemini" | "cohere" | "copilot" | string
-      -- provider = "copilot", -- Recommend using Claude
+      provider = "copilot", -- Recommend using Claude
       -- auto_suggestions_provider = "copilot", -- Since auto-suggestions are a high-frequency operation and therefore expensive, it is recommended to specify an inexpensive provider or even a free provider: copilot
       -- ---@type AvanteSupportedProvider
-      -- copilot = {
-      --   endpoint = "https://api.githubcopilot.com",
-      --   model = "gpt-4o-2024-08-06",
-      --   proxy = nil, -- [protocol://]host[:port] Use this proxy
-      --   allow_insecure = false, -- Allow insecure server connections
-      --   timeout = 30000, -- Timeout in milliseconds
-      --   temperature = 0,
-      --   max_tokens = 4096,
-      -- },
+      copilot = {
+        endpoint = "https://api.githubcopilot.com",
+        model = "gpt-4o-2024-08-06",
+        proxy = nil, -- [protocol://]host[:port] Use this proxy
+        allow_insecure = false, -- Allow insecure server connections
+        timeout = 30000, -- Timeout in milliseconds
+        temperature = 0,
+        max_tokens = 4096,
+      },
       ---Specify the special dual_boost mode
       ---1. enabled: Whether to enable dual_boost mode. Default to false.
       ---2. first_provider: The first provider to generate response. Default to "openai".
