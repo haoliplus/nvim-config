a6ba1c7291f580ddebe72af4024d47ae15904bcf
lihao <haoliplus@gmail.com>
2025-02-07 22:06:34 +0800

    Update config

--- lua/plugins/ai-helper.lua
+++ lua/plugins/ai-helper.lua
@@ -7,12 +7,13 @@ return {
         -- dir = "haoliplus/ai-helper", -- 本地插件目录路径
         -- dev = true, -- 标记为开发模式
         config = function()
+            -- dummy_prompts = require("aice_craft.prompts")
             require("aice_craft").setup({
                 api_key = os.getenv("DEEPSEEK_API_KEY"),
                 prompts = {
-                    -- ["改进代码"] = "请改进以下代码，并说明改进原因：",
-                    -- ["代码解释"] = "请解释以下代码的功能：",
-                    -- ["翻译"] = "请将以下内容翻译成英文：",
+                    ["改进代码"] = "请改进以下代码，并说明改进原因：",
+                    ["代码解释"] = "请解释以下代码的功能：",
+                    ["翻译"] = "请将以下内容翻译成英文：",
                     -- 添加更多自定义 prompts
                 }
             })
