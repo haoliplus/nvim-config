aebea529028c7d1c0301c02f86a368fc26df3f50
lihao <haoliplus@gmail.com>
2025-03-04 12:12:12 +0800

    Update filetype

--- lua/custom_filetype.lua
+++ lua/custom_filetype.lua
@@ -27,3 +27,41 @@ vim.filetype.add({
     end,
   },
 })
+
+-- Only for my specifical config file
+vim.api.nvim_create_autocmd("FileType", {
+  desc = "yaml config",
+  pattern = "yaml",
+  -- group = vim.api.nvim_create_augroup('black_on_save', { clear = true }),
+  callback = function(_)
+    vim.opt.cursorcolumn = false
+  end,
+})
+-- 为 JSON 文件添加 JSONC 检测
+vim.api.nvim_create_autocmd({"BufRead", "BufNewFile"}, {
+  pattern = "*.json",
+  callback = function()
+    -- 获取第一行内容
+    local first_line = vim.api.nvim_buf_get_lines(0, 0, 1, false)[1]
+
+    -- 检查第一行是否以注释开头
+    if first_line and (first_line:match("^%s*//") or first_line:match("^%s*/%*")) then
+      -- 如果是注释，设置文件类型为 jsonc
+      vim.bo.filetype = "jsonc"
+    else
+      -- 如果不是注释，保持为 json
+      vim.bo.filetype = "json"
+    end
+  end
+})
+
+vim.api.nvim_create_autocmd("FileType", {
+  desc = "Format python",
+  pattern = "python",
+  -- group = vim.api.nvim_create_augroup('black_on_save', { clear = true }),
+  callback = function(_)
+    vim.opt.shiftwidth = 4
+    vim.opt.tabstop = 2
+    vim.opt.softtabstop = 2
+  end,
+})
