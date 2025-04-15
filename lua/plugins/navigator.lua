1edc7f74926ebbe098762ed32dcba8152727791d
lihao <haoliplus@gmail.com>
2025-04-15 14:48:17 +0800

    Update bookmark

--- lua/plugins/navigator.lua
+++ lua/plugins/navigator.lua
@@ -178,13 +178,13 @@ return {
         keymap = {
           toggle = "<Leader><tab>", -- Toggle bookmarks(global keymap)
           close = "q", -- close bookmarks (buf keymap)
-          add = "\\z", -- Add bookmarks(global keymap)
+          add = "<Leader>z", -- Add bookmarks(global keymap)
           add_global = "\\g", -- Add global bookmarks(global keymap), global bookmarks will appear in all projects. Identified with the symbol '󰯾'
           jump = "<CR>", -- Jump from bookmarks(buf keymap)
           delete = "dd", -- Delete bookmarks(buf keymap)
           order = "<space><space>", -- Order bookmarks by frequency or updated_time(buf keymap)
           delete_on_virt = "\\dd", -- Delete bookmark at virt text line(global keymap)
-          show_desc = "\\sd", -- show bookmark desc(global keymap)
+          show_desc = "<Leader>sd", -- show bookmark desc(global keymap)
           focus_tags = "<c-j>", -- focus tags window
           focus_bookmarks = "<c-k>", -- focus bookmarks window
           toogle_focus = "<S-Tab>", -- toggle window focus (tags-window <-> bookmarks-window)
