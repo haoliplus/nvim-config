487cf4984c4d2a451641f24675d93342c85be39c
Hao Li <haoliplus@gmail.com>
2026-04-02 12:17:57 +0800

    remove treesitter in vim-illuminate

--- lua/plugins/theme.lua
+++ lua/plugins/theme.lua
@@ -182,6 +182,7 @@ return {
     "RRethy/vim-illuminate",
     event = { "BufReadPost", "BufNewFile" },
     opts = {
+      providers = { "lsp", "regex" },
       delay = 200,
       large_file_cutoff = 2000,
       large_file_overrides = {
